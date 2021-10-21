7287e0d6c30669ba411456f713aeb836743e3581
Ayushsingh07 <78909117+Ayushsingh07@users.noreply.github.com>
2021-10-21 13:03:11 +0530

    first time

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+  class MyApp extends StatelessWidget {
+    const MyApp({ Key? key }) ;
+  
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        home: Material(
+          child: Center(
+            child: Text("hello world"),),),
+      );
+    }
+  }
+}
