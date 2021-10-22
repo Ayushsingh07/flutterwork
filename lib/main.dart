7caef8b636d3b309e7c4933a11a12bc3875b3bb6
Ayushsingh07 <78909117+Ayushsingh07@users.noreply.github.com>
2021-10-22 13:04:10 +0530

    day2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/login.dart';
+import 'package:flutter_application_1/utils/routes.dart';
+import 'pages/homepage.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main() {
-  runApp(MyApp());
-  class MyApp extends StatelessWidget {
-    const MyApp({ Key? key }) ;
-  
-    @override
-    Widget build(BuildContext context) {
-      return MaterialApp(
-        home: Material(
-          child: Center(
-            child: Text("hello world"),),),
-      );
-    }
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      themeMode: ThemeMode.light,
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.red,
+        fontFamily: GoogleFonts.lato().fontFamily,
+        primaryTextTheme: GoogleFonts.latoTextTheme(),
+      ),
+      routes: {
+        "/": (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
+      },
+    );
   }
 }
